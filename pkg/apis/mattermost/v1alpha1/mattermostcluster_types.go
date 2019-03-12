00cc873d08742872952fbe82e3a9c275848c8583
Carlos Panato <ctadeu@gmail.com>
2019-03-12 18:45:55 +0100

    add initial spec

--- pkg/apis/mattermost/v1alpha1/mattermostcluster_types.go
+++ pkg/apis/mattermost/v1alpha1/mattermostcluster_types.go
@@ -1,26 +1,57 @@
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // MattermostClusterSpec defines the desired state of MattermostCluster
 // +k8s:openapi-gen=true
 type MattermostClusterSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	// Image defines the MattermostCluster Docker image.
+	Image string `json:"image,omitempty"`
+	// Version defines the MattermostCluster Docker image version.
+	Version string `json:"version,omitempty"`
+	// Replicas defines the number of Mattermost instances in a MattermostCluster resource
+	Replicas int32 `json:"replicas,omitempty"`
+	// IngressName defines the name to be used when creating the ingress rules
+	IngressName string `json:"ingressName"`
+	// NodeSelector is a selector which must be true for the pod to fit on a node.
+	// Selector which must match a node's labels for the pod to be scheduled on that node.
+	// More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
+	// +optional
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
+	// If specified, affinity will define the pod's scheduling constraints
+	// +optional
+	Affinity *corev1.Affinity `json:"affinity,omitempty"`
+
+	DatabaseType DatabaseType `json:"databaseType,omitempty"`
+}
+
+type DatabaseType struct {
+	Type string `json:"type,omitempty"`
 }
 
 // MattermostClusterStatus defines the observed state of MattermostCluster
 // +k8s:openapi-gen=true
 type MattermostClusterStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// Represents whether any actions on the underlaying managed objects are
+	// being performed. Only delete actions will be performed.
+	Paused bool `json:"paused"`
+	// Total number of non-terminated pods targeted by this Mattermost deployment
+	// (their labels match the selector).
+	Replicas int32 `json:"replicas"`
+	// Total number of non-terminated pods targeted by this Mattermost deployment
+	// that have the desired version spec.
+	UpdatedReplicas int32 `json:"updatedReplicas"`
+	// Total number of available pods (ready for at least minReadySeconds)
+	// targeted by this Mattermost deployment.
+	AvailableReplicas int32 `json:"availableReplicas"`
+	// Total number of unavailable pods targeted by this Mattermost deployment.
+	UnavailableReplicas int32 `json:"unavailableReplicas"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -28,11 +59,19 @@ type MattermostClusterStatus struct {
 // MattermostCluster is the Schema for the mattermostclusters API
 // +k8s:openapi-gen=true
 type MattermostCluster struct {
-	metav1.TypeMeta   `json:",inline"`
+	metav1.TypeMeta `json:",inline"`
+	// Standard object’s metadata. More info:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#metadata
+	// +k8s:openapi-gen=false
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   MattermostClusterSpec   `json:"spec,omitempty"`
-	Status MattermostClusterStatus `json:"status,omitempty"`
+	// Specification of the desired behavior of the Mattermost cluster. More info:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#spec-and-status
+	Spec MattermostClusterSpec `json:"spec"`
+	// Most recent observed status of the Mattermost cluster. Read-only. Not
+	// included when requesting from the apiserver, only from the Mattermost
+	// Operator API itself. More info:
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#spec-and-status
+	Status *MattermostClusterStatus `json:"status,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
