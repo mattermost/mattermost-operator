5b44a41bce8afd674ab2a6bc76df4456c52d21b1
Carlos Panato <ctadeu@gmail.com>
2019-03-12 18:45:38 +0100

    make cluster wide

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/mattermost/mattermost-operator/pkg/controller"
 	"github.com/mattermost/mattermost-operator/version"
 
-	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	"github.com/operator-framework/operator-sdk/pkg/leader"
 	"github.com/operator-framework/operator-sdk/pkg/log/zap"
 	"github.com/operator-framework/operator-sdk/pkg/metrics"
@@ -61,12 +60,6 @@ func main() {
 
 	printVersion()
 
-	namespace, err := k8sutil.GetWatchNamespace()
-	if err != nil {
-		log.Error(err, "Failed to get watch namespace")
-		os.Exit(1)
-	}
-
 	// Get a config to talk to the apiserver
 	cfg, err := config.GetConfig()
 	if err != nil {
@@ -85,7 +78,7 @@ func main() {
 
 	// Create a new Cmd to provide shared dependencies and start components
 	mgr, err := manager.New(cfg, manager.Options{
-		Namespace:          namespace,
+		Namespace:          "",
 		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
 	})
 	if err != nil {
