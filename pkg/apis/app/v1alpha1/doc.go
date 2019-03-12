d2e7758c0865f90390c1ede0e0dc9407765dd4d8
Carlos Panato <ctadeu@gmail.com>
2019-03-12 10:28:35 +0100

    replace app.mattermost.com just to mattermost.com

--- pkg/apis/app/v1alpha1/doc.go
+++ pkg/apis/app/v1alpha1/doc.go
@@ -1,4 +1,4 @@
 // Package v1alpha1 contains API Schema definitions for the app v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=app.mattermost.com
+// +groupName=mattermost.com
 package v1alpha1
--- pkg/apis/app/v1alpha1/register.go
+++ pkg/apis/app/v1alpha1/register.go
@@ -2,7 +2,7 @@
 
 // Package v1alpha1 contains API Schema definitions for the app v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=app.mattermost.com
+// +groupName=mattermost.com
 package v1alpha1
 
 import (
@@ -12,7 +12,7 @@ import (
 
 var (
 	// SchemeGroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: "app.mattermost.com", Version: "v1alpha1"}
+	SchemeGroupVersion = schema.GroupVersion{Group: "mattermost.com", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
