79c4debbac3708c6289e4fc2389a935a0d8e4dbd
Carlos Panato <ctadeu@gmail.com>
2019-04-01 16:25:51 +0200

    update per feedback and add initial test for controller

--- pkg/apis/mattermost/v1alpha1/clusterinstallation_types.go
+++ pkg/apis/mattermost/v1alpha1/clusterinstallation_types.go
@@ -5,14 +5,14 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// MattermostClusterSpec defines the desired state of MattermostCluster
+// ClusterInstallationSpec defines the desired state of ClusterInstallation
 // +k8s:openapi-gen=true
-type MattermostClusterSpec struct {
-	// Image defines the MattermostCluster Docker image.
+type ClusterInstallationSpec struct {
+	// Image defines the ClusterInstallation Docker image.
 	Image string `json:"image,omitempty"`
-	// Version defines the MattermostCluster Docker image version.
+	// Version defines the ClusterInstallation Docker image version.
 	Version string `json:"version,omitempty"`
-	// Replicas defines the number of Mattermost instances in a MattermostCluster resource
+	// Replicas defines the number of Mattermost instances in a ClusterInstallation resource
 	Replicas int32 `json:"replicas,omitempty"`
 	// IngressName defines the name to be used when creating the ingress rules
 	IngressName string `json:"ingressName"`
@@ -32,33 +32,25 @@ type DatabaseType struct {
 	Type string `json:"type,omitempty"`
 }
 
-// MattermostClusterStatus defines the observed state of MattermostCluster
+// ClusterInstallationStatus defines the observed state of ClusterInstallation
 // +k8s:openapi-gen=true
-type MattermostClusterStatus struct {
+type ClusterInstallationStatus struct {
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 
-	// Represents whether any actions on the underlaying managed objects are
+	// Represents whether any actions on the underlying managed objects are
 	// being performed. Only delete actions will be performed.
 	Paused bool `json:"paused"`
 	// Total number of non-terminated pods targeted by this Mattermost deployment
 	// (their labels match the selector).
 	Replicas int32 `json:"replicas"`
-	// Total number of non-terminated pods targeted by this Mattermost deployment
-	// that have the desired version spec.
-	UpdatedReplicas int32 `json:"updatedReplicas"`
-	// Total number of available pods (ready for at least minReadySeconds)
-	// targeted by this Mattermost deployment.
-	AvailableReplicas int32 `json:"availableReplicas"`
-	// Total number of unavailable pods targeted by this Mattermost deployment.
-	UnavailableReplicas int32 `json:"unavailableReplicas"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// MattermostCluster is the Schema for the mattermostclusters API
+// ClusterInstallation is the Schema for the clusterinstallations API
 // +k8s:openapi-gen=true
-type MattermostCluster struct {
+type ClusterInstallation struct {
 	metav1.TypeMeta `json:",inline"`
 	// Standard object’s metadata. More info:
 	// https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#metadata
@@ -66,23 +58,23 @@ type MattermostCluster struct {
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 	// Specification of the desired behavior of the Mattermost cluster. More info:
 	// https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#spec-and-status
-	Spec MattermostClusterSpec `json:"spec"`
+	Spec ClusterInstallationSpec `json:"spec"`
 	// Most recent observed status of the Mattermost cluster. Read-only. Not
 	// included when requesting from the apiserver, only from the Mattermost
 	// Operator API itself. More info:
 	// https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#spec-and-status
-	Status *MattermostClusterStatus `json:"status,omitempty"`
+	Status *ClusterInstallationStatus `json:"status,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// MattermostClusterList contains a list of MattermostCluster
-type MattermostClusterList struct {
+// ClusterInstallationList contains a list of ClusterInstallation
+type ClusterInstallationList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []MattermostCluster `json:"items"`
+	Items           []ClusterInstallation `json:"items"`
 }
 
 func init() {
-	SchemeBuilder.Register(&MattermostCluster{}, &MattermostClusterList{})
+	SchemeBuilder.Register(&ClusterInstallation{}, &ClusterInstallationList{})
 }
