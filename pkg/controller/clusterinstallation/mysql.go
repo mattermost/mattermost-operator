2c3790a29c83fbdfae9395db14676ad10cf5bdb6
Witold Konior <witold.konior@formulate.se>
2019-09-07 10:23:26 +0200

    update mysql cluster with objectmatcher

--- pkg/controller/clusterinstallation/mysql.go
+++ pkg/controller/clusterinstallation/mysql.go
@@ -3,7 +3,6 @@ package clusterinstallation
 import (
 	"context"
 	"fmt"
-	"reflect"
 
 	"github.com/go-logr/logr"
 	mysqlOperator "github.com/presslabs/mysql-operator/pkg/apis/mysql/v1alpha1"
@@ -18,39 +17,20 @@ import (
 
 func (r *ReconcileClusterInstallation) checkMySQLCluster(mattermost *mattermostv1alpha1.ClusterInstallation, reqLogger logr.Logger) error {
 	reqLogger = reqLogger.WithValues("Reconcile", "mysql")
-	cluster := mattermostmysql.Cluster(mattermost)
+	desired := mattermostmysql.Cluster(mattermost)
 
-	err := r.createMySQLClusterIfNotExists(mattermost, cluster, reqLogger)
+	err := r.createMySQLClusterIfNotExists(mattermost, desired, reqLogger)
 	if err != nil {
 		return err
 	}
 
-	foundCluster := &mysqlOperator.MysqlCluster{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: cluster.Name, Namespace: cluster.Namespace}, foundCluster)
+	current := &mysqlOperator.MysqlCluster{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: desired.Name, Namespace: desired.Namespace}, current)
 	if err != nil {
 		return err
 	}
 
-	var update bool
-
-	updatedLabels := ensureLabels(cluster.Labels, foundCluster.Labels)
-	if !reflect.DeepEqual(updatedLabels, foundCluster.Labels) {
-		reqLogger.Info("Updating mysql cluster labels")
-		foundCluster.Labels = updatedLabels
-		update = true
-	}
-
-	if !reflect.DeepEqual(cluster.Spec, foundCluster.Spec) {
-		reqLogger.Info("Updating mysql cluster spec")
-		foundCluster.Spec = cluster.Spec
-		update = true
-	}
-
-	if update {
-		return r.client.Update(context.TODO(), foundCluster)
-	}
-
-	return nil
+	return r.update(current, desired, reqLogger)
 }
 
 func (r *ReconcileClusterInstallation) createMySQLClusterIfNotExists(mattermost *mattermostv1alpha1.ClusterInstallation, cluster *mysqlOperator.MysqlCluster, reqLogger logr.Logger) error {
