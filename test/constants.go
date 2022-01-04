19e3fd405e581ccacf431522444a6947b70f8c5c
Stavros Foteinopoulos <stafot@gmail.com>
2022-01-04 17:50:50 +0200

    fix stable version

--- test/constants.go
+++ test/constants.go
@@ -3,9 +3,9 @@ package test
 const (
 	// LatestStableMattermostVersion is the most recent stable version of
 	// Mattermost.
-	LatestStableMattermostVersion = "6.1.0"
+	LatestStableMattermostVersion = "6.2.1"
 	// PreviousStableMattermostVersion is the latest dot release of Mattermost
 	// that is one minor version lower than the latest release.
 	// i.e. It's a typical release that would need to be upgraded from.
-	PreviousStableMattermostVersion = "6.0.3"
+	PreviousStableMattermostVersion = "6.2.0"
 )
