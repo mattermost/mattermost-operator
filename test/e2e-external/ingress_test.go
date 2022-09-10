b279e85cae66a4433e8a63043f1abfd4d0d4cbe7
Felipe Martin <me@fmartingr.com>
2022-09-10 20:11:19 +0200

    test(e2e): ingress is deleted when disabled

--- test/e2e-external/ingress_test.go
+++ test/e2e-external/ingress_test.go
@@ -1,13 +1,15 @@
 package e2e
 
 import (
+	"context"
 	"testing"
 
 	mmv1beta "github.com/mattermost/mattermost-operator/apis/mattermost/v1beta1"
 	pkgUtils "github.com/mattermost/mattermost-operator/pkg/utils"
 	"github.com/mattermost/mattermost-operator/test/e2e"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	networkingv1 "k8s.io/api/networking/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -59,10 +61,68 @@ func TestMattermostCustomIngressOk(t *testing.T) {
 	clusterMattermost := instance.Get()
 
 	// Check entire ingress object
-	assert.Equal(t, mattermost.Spec.Ingress, clusterMattermost.Spec.Ingress, "Mattermost Ingress spec should be the same as defined")
+	require.Equal(t, mattermost.Spec.Ingress, clusterMattermost.Spec.Ingress, "Mattermost Ingress spec should be the same as defined")
 
 	// Check specific attributes individually, in case deep equality fails for wathever reason
-	assert.Equal(t, mattermost.Spec.Ingress.Hosts, clusterMattermost.Spec.Ingress.Hosts, "Spec should have same hosts defined")
-	assert.Equal(t, mattermost.Spec.Ingress.Annotations, clusterMattermost.Spec.Ingress.Annotations, "Spec should contain specified annotations")
-	assert.Equal(t, mattermost.Spec.Ingress.IngressClass, clusterMattermost.Spec.Ingress.IngressClass, "Spec should contain the same ingress class")
+	require.Equal(t, mattermost.Spec.Ingress.Hosts, clusterMattermost.Spec.Ingress.Hosts, "Spec should have same hosts defined")
+	require.Equal(t, mattermost.Spec.Ingress.Annotations, clusterMattermost.Spec.Ingress.Annotations, "Spec should contain specified annotations")
+	require.Equal(t, mattermost.Spec.Ingress.IngressClass, clusterMattermost.Spec.Ingress.IngressClass, "Spec should contain the same ingress class")
+}
+
+func TestMattermostIngressDisableOk(t *testing.T) {
+	namespace := "e2e-test-ingress-disable"
+	name := "test-mm"
+
+	testEnv, err := SetupTestEnv(k8sClient, namespace)
+	require.NoError(t, err)
+	defer testEnv.CleanupFunc()
+
+	mattermost := &mmv1beta.Mattermost{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: mmv1beta.MattermostSpec{
+			Ingress: &mmv1beta.Ingress{
+				Enabled: true,
+				Host:    namespace + ".mattermost.dev",
+			},
+			Replicas: pkgUtils.NewInt32(1),
+			FileStore: mmv1beta.FileStore{
+				External: &testEnv.FileStoreConfig,
+			},
+			Database: mmv1beta.Database{
+				External: &testEnv.DBConfig,
+			},
+		},
+	}
+
+	ctx := context.Background()
+	instance := e2e.NewMattermostInstance(t, k8sClient, mattermost)
+	defer instance.Destroy()
+
+	instance.CreateAndWait()
+
+	// Ensure the instance is created and with the ingress enabled and ingress object created
+	clusterMattermost := instance.Get()
+	require.NotNil(t, clusterMattermost.Spec.Ingress, "ingress should be defined")
+	require.True(t, clusterMattermost.IngressEnabled(), "ingress should be enabled at the beginning")
+
+	// Check that the ingress object is created
+	var mmIngress networkingv1.Ingress
+	err = k8sClient.Get(ctx, instance.Namespace(), &mmIngress)
+	require.NoError(t, err, "Ingress should be present in cluster")
+	require.Equal(t, name, mmIngress.Name)
+
+	// Disable the ingress and update the instance
+	clusterMattermost.Spec.Ingress.Enabled = false
+	instance.UpdateAndWait(&clusterMattermost)
+
+	// Ensure the ingress object has been removed
+	err = k8sClient.Get(ctx, instance.Namespace(), &mmIngress)
+	require.True(t, k8serrors.IsNotFound(err), "Ingress should be deleted after object update")
+
+	// Ensure the mattermost instance has the ingress disabled
+	clusterMattermost = instance.Get()
+	require.False(t, clusterMattermost.IngressEnabled(), "ingress should be disabled after resource update")
 }
