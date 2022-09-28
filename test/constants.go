281c0d966c0deb08773a2ec39f8d174c23fa2606
Mir Shahriar Sabuj <shahriar052@gmail.com>
2022-09-28 17:35:24 +0600

    update version to latest one

--- test/constants.go
+++ test/constants.go
@@ -3,9 +3,9 @@ package test
 const (
 	// LatestStableMattermostVersion is the most recent stable version of
 	// Mattermost.
-	LatestStableMattermostVersion = "6.5.0"
+	LatestStableMattermostVersion = "7.3.0"
 	// PreviousStableMattermostVersion is the latest dot release of Mattermost
 	// that is one minor version lower than the latest release.
 	// i.e. It's a typical release that would need to be upgraded from.
-	PreviousStableMattermostVersion = "6.2.1"
+	PreviousStableMattermostVersion = "7.2.0"
 )
