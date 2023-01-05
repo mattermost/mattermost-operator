ea62d3552aecb3afa28c8a21c1a2ed78daec9a2b
Mir Shahriar Sabuj <mir.shahriar@mattermost.com>
2023-01-05 21:15:35 +0600

    Load docker images of linux/x86_64 platform

--- test/setup_test.sh
+++ test/setup_test.sh
@@ -5,14 +5,17 @@ set -Eeuxo pipefail
 # Move the operator container inside Kind container so that the image is
 # available to the docker in docker environment.
 # Copy the image to the cluster to make a bit more fast to start
-docker pull quay.io/presslabs/mysql-operator:0.4.0
-docker pull quay.io/presslabs/mysql-operator-sidecar:0.4.0
-docker pull quay.io/presslabs/mysql-operator-orchestrator:0.4.0
+docker pull --platform=linux/x86_64 quay.io/presslabs/mysql-operator:0.4.0
+docker pull --platform=linux/x86_64 quay.io/presslabs/mysql-operator-sidecar:0.4.0
+docker pull --platform=linux/x86_64 quay.io/presslabs/mysql-operator-orchestrator:0.4.0
+docker pull --platform=linux/x86_64 percona:5.7.26
+
 docker pull minio/k8s-operator:1.0.7
 
 kind load docker-image quay.io/presslabs/mysql-operator:0.4.0
 kind load docker-image quay.io/presslabs/mysql-operator-sidecar:0.4.0
 kind load docker-image quay.io/presslabs/mysql-operator-orchestrator:0.4.0
+kind load docker-image percona:5.7.26
 kind load docker-image minio/k8s-operator:1.0.7
 sleep 10
 
