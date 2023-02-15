706bd124ccdd408bb2b5ffc10085c8f9f3d3bb26
Felipe Martin <me@fmartingr.com>
2023-02-15 13:02:16 +0100

    Set mysql version only for mysql database type

--- apis/mattermost/v1alpha1/clusterinstallation_utils.go
+++ apis/mattermost/v1alpha1/clusterinstallation_utils.go
@@ -26,9 +26,9 @@ const (
 	DefaultStorageSize = "50Gi"
 	// DefaultPullPolicy is ifNotPresent
 	DefaultPullPolicy = corev1.PullIfNotPresent
-	// DefaultDatabaseVersion
+	// DefaultMysqlDatabaseVersion
 	// https://github.com/bitpoke/mysql-operator/blob/master/pkg/util/constants/constants.go#L87
-	DefaultDatabaseVersion = "8.0"
+	DefaultMysqlDatabaseVersion = "8.0"
 
 	// ClusterLabel is the label applied across all compoments
 	ClusterLabel = "v1alpha1.mattermost.com/installation"
@@ -144,8 +144,8 @@ func (db *Database) SetDefaults() {
 	if db.StorageSize == "" {
 		db.StorageSize = DefaultStorageSize
 	}
-	if db.Version == "" {
-		db.Version = DefaultDatabaseVersion
+	if db.Type == DefaultMattermostDatabaseType && db.Version == "" {
+		db.Version = DefaultMysqlDatabaseVersion
 	}
 }
 
