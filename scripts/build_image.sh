cd7a2c3f4975564414d7955db85399736a42e692
Felipe Martin <me@fmartingr.com>
2023-04-17 13:10:09 +0200

    build default to local

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -5,22 +5,23 @@ set -o nounset  # exit immediately if a variable is unset
 set -o pipefail # if any command in a pipeline fails, the return status is the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully
 set -o xtrace   # print each command before executing it
 
-DOCKER_COMMAND=("buildx" "build")
-EXTRA_FLAGS=("--no-cache" "--platform linux/amd64,linux/arm64" "--push")
-
-if [[ $(docker buildx ls | grep -c operator-builder) -eq 0 ]]; then
-    docker buildx create --use --name operator-builder
-fi
+DOCKER_COMMAND=("build")
+EXTRA_FLAGS=("--no-cache")
 
 # if the PUSH_TAG environment variable is provided, tag the image with it as well.
 if [[ -n "${PUSH_TAG:-}" ]]; then
     EXTRA_FLAGS+=("-t ${OPERATOR_IMAGE}:${PUSH_TAG}")
 fi
 
-# If the image is going to be built locally (first argument is "local")
-if [[ "$1" == "local" ]]; then
-    DOCKER_COMMAND=("build")
-    EXTRA_FLAGS=("--no-cache")
+# If the image is going to be built using buildx
+if [[ "$1" == "buildx" ]]; then
+    DOCKER_COMMAND=("buildx" "build")
+    EXTRA_FLAGS=("--no-cache" "--platform linux/amd64,linux/arm64" "--push")
+
+    # Set up buildx builder if it's not already set up
+    if [[ $(docker buildx ls | grep -c operator-builder) -eq 0 ]]; then
+        docker buildx create --use --name operator-builder
+    fi
 fi
 
 docker "${DOCKER_COMMAND[@]}" \
