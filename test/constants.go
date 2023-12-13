829dfe84ef83fdde83d21b227e0d218b47efa963
Gabe Jackson <3694686+gabrieljackson@users.noreply.github.com>
2023-12-13 11:50:40 -0500

    Bump default Mattermost server version (#368)
    The default has been updated to v9.2.3

--- test/constants.go
+++ test/constants.go
@@ -3,9 +3,9 @@ package test
 const (
 	// LatestStableMattermostVersion is the most recent stable version of
 	// Mattermost.
-	LatestStableMattermostVersion = "7.5.1"
+	LatestStableMattermostVersion = "9.2.3"
 	// PreviousStableMattermostVersion is the latest dot release of Mattermost
 	// that is one minor version lower than the latest release.
 	// i.e. It's a typical release that would need to be upgraded from.
-	PreviousStableMattermostVersion = "7.4.0"
+	PreviousStableMattermostVersion = "9.1.0"
 )
