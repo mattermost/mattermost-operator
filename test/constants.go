9966684d6706633bbce85427942c3b66a822388c
Gabe Jackson <3694686+gabrieljackson@users.noreply.github.com>
2024-04-30 09:22:06 -0400

    Bump operator version to v1.22 (#380)
    This also updates the default Mattermost version to 9.7.3.

--- test/constants.go
+++ test/constants.go
@@ -3,9 +3,9 @@ package test
 const (
 	// LatestStableMattermostVersion is the most recent stable version of
 	// Mattermost.
-	LatestStableMattermostVersion = "9.5.1"
+	LatestStableMattermostVersion = "9.7.3"
 	// PreviousStableMattermostVersion is the latest dot release of Mattermost
 	// that is one minor version lower than the latest release.
 	// i.e. It's a typical release that would need to be upgraded from.
-	PreviousStableMattermostVersion = "9.4.0"
+	PreviousStableMattermostVersion = "9.6.0"
 )
