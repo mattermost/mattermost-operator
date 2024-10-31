dfa6165768806c8d0febc7bc94a8cb5c5e837a6b
Nick Misasi <nick.misasi@mattermost.com>
2024-10-31 14:18:25 -0400

    Use temp output dir

--- scripts/k8s.io/code-generator/generate-groups.sh
+++ scripts/k8s.io/code-generator/generate-groups.sh
@@ -80,20 +80,24 @@ fi
 
 if [ "${GENS}" = "all" ] || grep -qw "client" <<<"${GENS}"; then
   echo "Generating clientset for ${GROUPS_WITH_VERSIONS} at ${OUTPUT_PKG}/${CLIENTSET_PKG_NAME:-clientset}"
+  TEMP_DIR=$(mktemp -d)
+  trap 'rm -rf "${TEMP_DIR}"' EXIT
   "${gobin}/client-gen" \
       --clientset-name "${CLIENTSET_NAME_VERSIONED:-versioned}" \
       --input-base "" \
       --input "$(codegen::join , "${FQ_APIS[@]}")" \
       --output-pkg "${OUTPUT_PKG}/${CLIENTSET_PKG_NAME:-clientset}" \
-      --output-dir "${OUTPUT_PKG}" \
+      --output-dir "${TEMP_DIR}" \
       "$@"
 fi
 
 if [ "${GENS}" = "all" ] || grep -qw "lister" <<<"${GENS}"; then
   echo "Generating listers for ${GROUPS_WITH_VERSIONS} at ${OUTPUT_PKG}/listers"
+  TEMP_DIR=$(mktemp -d)
+  trap 'rm -rf "${TEMP_DIR}"' EXIT
   "${gobin}/lister-gen" \
       --output-pkg "${OUTPUT_PKG}/listers" \
-      --output-dir "${OUTPUT_PKG}" \
+      --output-dir "${TEMP_DIR}" \
       "$(codegen::join , "${FQ_APIS[@]}")" \
       "$@"
 fi
@@ -101,12 +105,14 @@ fi
 
 if [ "${GENS}" = "all" ] || grep -qw "informer" <<<"${GENS}"; then
   echo "Generating informers for ${GROUPS_WITH_VERSIONS} at ${OUTPUT_PKG}/informers"
+  TEMP_DIR=$(mktemp -d)
+  trap 'rm -rf "${TEMP_DIR}"' EXIT
   for FQ_API in "${FQ_APIS[@]}"; do 
     "${gobin}/informer-gen" \
         --versioned-clientset-package "${OUTPUT_PKG}/${CLIENTSET_PKG_NAME:-clientset}/${CLIENTSET_NAME_VERSIONED:-versioned}" \
         --listers-package "${OUTPUT_PKG}/listers" \
         --output-pkg "${OUTPUT_PKG}/informers" \
-        --output-dir "${OUTPUT_PKG}" \
+        --output-dir "${TEMP_DIR}" \
         "${FQ_API}" \
         "$@"
   done
